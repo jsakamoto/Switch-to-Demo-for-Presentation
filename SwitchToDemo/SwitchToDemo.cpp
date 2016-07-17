24167cc2790e5462075aa81a747345bc944feb8d
jsakamoto <junichi.jp@gmail.com>
2016-07-17 20:52:26 +0900

    Apply auto format.

--- SwitchToDemo/SwitchToDemo.cpp
+++ SwitchToDemo/SwitchToDemo.cpp
@@ -26,14 +26,14 @@ void OnHotKey(HWND);
 void OnTaskTrayMessage(HWND hWnd, LPARAM lParam);
 HWND FindApplicationWindow();
 HWND FindSlideShowWindow();
-BOOL CALLBACK FindPowerPointSlideShowWindiwProc(HWND hwnd , LPARAM lParam);
+BOOL CALLBACK FindPowerPointSlideShowWindiwProc(HWND hwnd, LPARAM lParam);
 void FadeMainWnd(BOOL bFadeIn);
 void Configuration(HWND hwnd);
 
 int APIENTRY wWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
@@ -51,7 +51,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
 	MyRegisterClass(hInstance);
 
 	// Initialize applicationinstance.
-	if (!InitInstance (hInstance, nCmdShow))
+	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
@@ -64,7 +64,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
 		DispatchMessage(&msg);
 	}
 
-	return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -74,17 +74,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SWITCHTODEMO));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
-	wcex.lpszMenuName	= NULL;//MAKEINTRESOURCE(IDC_SWITCHTODEMO);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SWITCHTODEMO));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SWITCHTODEMO));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.lpszMenuName = NULL;//MAKEINTRESOURCE(IDC_SWITCHTODEMO);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SWITCHTODEMO));
 
 	return RegisterClassEx(&wcex);
 }
@@ -98,22 +98,22 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	lstrcat(szConfigPath, TEXT(".ini"));
 
 	// Load configuration.
-	g_Config.Hotkey = GetPrivateProfileInt(pszConfigSection, TEXT("Hotkey"), MAKEWORD('H', MOD_WIN|MOD_SHIFT), szConfigPath);
-	GetPrivateProfileString(pszConfigSection, TEXT("HotkeyDisplayName"), TEXT("Win + Shift + H"), g_Config.szHotkeyDisplayName, sizeof(g_Config.szHotkeyDisplayName)/sizeof(TCHAR), szConfigPath);
+	g_Config.Hotkey = GetPrivateProfileInt(pszConfigSection, TEXT("Hotkey"), MAKEWORD('H', MOD_WIN | MOD_SHIFT), szConfigPath);
+	GetPrivateProfileString(pszConfigSection, TEXT("HotkeyDisplayName"), TEXT("Win + Shift + H"), g_Config.szHotkeyDisplayName, sizeof(g_Config.szHotkeyDisplayName) / sizeof(TCHAR), szConfigPath);
 
 	// Create main window.
 	hInst = hInstance;
 	hMainWnd = CreateWindowEx(
-	   WS_EX_LAYERED|WS_EX_TOPMOST|WS_EX_TOOLWINDOW, 
-	   szWindowClass, szTitle, WS_POPUP,
-	   0, 0, 640, 480, NULL, NULL, hInstance, NULL);
+		WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+		szWindowClass, szTitle, WS_POPUP,
+		0, 0, 640, 480, NULL, NULL, hInstance, NULL);
 	if (!hMainWnd) return FALSE;
 
 	// Register Hot-key "Windows + Shift + H".
 	BYTE mod = HIBYTE(g_Config.Hotkey);
 	BYTE vkey = LOBYTE(g_Config.Hotkey);
 	RegisterHotKey(hMainWnd, 0, mod, vkey);
-   
+
 	// Setup task tray icon.
 	SetupTaskTrayIcon(hMainWnd);
 
@@ -131,12 +131,12 @@ void SetupTaskTrayIcon(HWND hWnd)
 
 	Shell_NotifyIcon(NIM_DELETE, &nid);
 
-	nid.hIcon = (HICON)LoadImage(hInst,MAKEINTRESOURCE(IDI_SWITCHTODEMO),IMAGE_ICON,16,16,LR_SHARED);
+	nid.hIcon = (HICON)LoadImage(hInst, MAKEINTRESOURCE(IDI_SWITCHTODEMO), IMAGE_ICON, 16, 16, LR_SHARED);
 	nid.uCallbackMessage = WM_TRAYICONMESSAGE;
 	lstrcpy(nid.szInfoTitle, szTitle);
 	wsprintf(nid.szTip, TEXT("%s (Hotkey is %s)"), szTitle, g_Config.szHotkeyDisplayName);
 	wsprintf(nid.szInfo, TEXT("Enter hot-key \"%s\" to Show or Hide Slide-show."), g_Config.szHotkeyDisplayName);
-	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE|NIF_INFO;
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE | NIF_INFO;
 	Shell_NotifyIcon(NIM_ADD, &nid);
 }
 
@@ -149,7 +149,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
+		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		switch (wmId)
 		{
@@ -163,12 +163,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
-	
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		EndPaint(hWnd, &ps);
 		break;
-	
+
 	case WM_TRAYICONMESSAGE:
 		OnTaskTrayMessage(hWnd, lParam);
 		break;
@@ -176,27 +176,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		SetupTaskTrayIcon(hWnd);
 		break;
 	case WM_DESTROY:
+	{
+		HWND hSlideShowWnd = FindSlideShowWindow();
+		if (IsWindow(hSlideShowWnd) && IsWindowVisible(hSlideShowWnd) == FALSE)
 		{
-			HWND hSlideShowWnd = FindSlideShowWindow();
-			if (IsWindow(hSlideShowWnd) && IsWindowVisible(hSlideShowWnd) == FALSE)
-			{
-				ShowWindow(hSlideShowWnd, SW_SHOW);
-				SetForegroundWindow(hSlideShowWnd);
-			}
-		
-			NOTIFYICONDATA nid;
-			ZeroMemory(&nid, sizeof(nid));
-			nid.cbSize = sizeof(nid);
-			nid.hWnd = hWnd;
-			Shell_NotifyIcon(NIM_DELETE, &nid);
+			ShowWindow(hSlideShowWnd, SW_SHOW);
+			SetForegroundWindow(hSlideShowWnd);
 		}
-		PostQuitMessage(0);
-		break;
-	
+
+		NOTIFYICONDATA nid;
+		ZeroMemory(&nid, sizeof(nid));
+		nid.cbSize = sizeof(nid);
+		nid.hWnd = hWnd;
+		Shell_NotifyIcon(NIM_DELETE, &nid);
+	}
+	PostQuitMessage(0);
+	break;
+
 	case WM_HOTKEY:
 		OnHotKey(hWnd);
 		break;
-	
+
 	default:
 		if (message == WM_TASKBARCREATED)
 		{
@@ -227,27 +227,27 @@ HWND FindSlideShowWindow()
 	// Microsoft PowerPoint (2007 Viewer, 2010-2013) 
 	HWND hSlideShowWnd = NULL;
 	EnumWindows(FindPowerPointSlideShowWindiwProc, (LPARAM)&hSlideShowWnd);
-	
+
 	if (IsWindow(hSlideShowWnd) == FALSE)
 		// Kingsoft Presentation (2010) 
 		hSlideShowWnd = FindWindow(TEXT("TSlideShowDlg.UnicodeClass"), NULL);
 	return hSlideShowWnd;
 }
 
-BOOL CALLBACK FindPowerPointSlideShowWindiwProc(HWND hwnd , LPARAM lParam)
+BOOL CALLBACK FindPowerPointSlideShowWindiwProc(HWND hwnd, LPARAM lParam)
 {
 	// Find by Window Class Name...
 	TCHAR szClassName[50];
-	GetClassName(hwnd, szClassName, sizeof(szClassName)/sizeof(TCHAR));
-	if(lstrcmp(szClassName, TEXT("screenClass")) != 0) return TRUE;
+	GetClassName(hwnd, szClassName, sizeof(szClassName) / sizeof(TCHAR));
+	if (lstrcmp(szClassName, TEXT("screenClass")) != 0) return TRUE;
 
 	// But exclude Presenter View.
 	TCHAR szCaption[400];
-	GetWindowText(hwnd, szCaption, sizeof(szCaption)/sizeof(TCHAR));
+	GetWindowText(hwnd, szCaption, sizeof(szCaption) / sizeof(TCHAR));
 	LPCTSTR pNGWord = TEXT("Presenter View");
 	int begin = lstrlen(szCaption) - lstrlen(pNGWord);
 	begin = max(0, begin);
-	if(lstrcmp(&szCaption[begin], pNGWord) == 0) return TRUE;
+	if (lstrcmp(&szCaption[begin], pNGWord) == 0) return TRUE;
 
 	HWND* phSlideShowWnd = (HWND*)lParam;
 	*phSlideShowWnd = hwnd;
@@ -268,13 +268,13 @@ void OnHotKey(HWND hWnd)
 {
 	HWND hAppWnd = FindApplicationWindow();
 	HWND hSlideShowWnd = FindSlideShowWindow();
-	
+
 	// Minimize application window.
 	if (IsWindow(hAppWnd) && IsWindow(hSlideShowWnd))
 	{
 		ShowWindow(hAppWnd, SW_SHOWMINNOACTIVE);
 	}
-	
+
 	// Hide or Show slide-show window.
 	if (IsWindow(hSlideShowWnd))
 	{
@@ -306,14 +306,14 @@ void FadeMainWnd(BOOL bFadeIn)
 		SetForegroundWindow(hMainWnd);
 		BringWindowToTop(hMainWnd);
 	}
-	for(int i=0; i<256; i+=16)
+	for (int i = 0; i < 256; i += 16)
 	{
 		int alpha = abs(base - i);
 		SetLayeredWindowAttributes(hMainWnd, 0, alpha, LWA_ALPHA);
 		UpdateWindow(hMainWnd);
 		Sleep(30);
 	}
-	if(!bFadeIn)
+	if (!bFadeIn)
 	{
 		ShowWindow(hMainWnd, SW_HIDE);
 	}
@@ -325,7 +325,7 @@ void Configuration(HWND hwnd)
 	UnregisterHotKey(hwnd, 0);
 
 	INT result = DialogBox(hInst, MAKEINTRESOURCE(IDD_CONFIG), GetDesktopWindow(), ConfigDialogProc);
-	if(result == IDOK)
+	if (result == IDOK)
 	{
 		// Save configuration.
 		TCHAR szHotkeyInt[100];
